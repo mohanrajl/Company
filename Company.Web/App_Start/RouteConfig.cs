9649bd72b6544d99cd29ce43a8c25a8aac2efd03
Mohanraj Lakshmikanthan <mohanrajl@users.noreply.github.com>
2019-02-21 17:30:26 +0000

    Update RouteConfig.cs

--- Company.Web/App_Start/RouteConfig.cs
+++ Company.Web/App_Start/RouteConfig.cs
@@ -12,6 +12,12 @@ namespace Company.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(
+                name: "UserFolder",
+                url: "secure/{controller}/{action}/{id}",
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
                 name: "Default",
