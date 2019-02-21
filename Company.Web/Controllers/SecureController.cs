4138bad7b4c2eb877be52b4004f7478b75003ebd
Mohanraj Lakshmikanthan <mohanrajl@users.noreply.github.com>
2019-02-21 16:43:18 +0000

    Add files via upload

---
+++ Company.Web/Controllers/SecureController.cs
@@ -0,0 +1,46 @@
+using System.Web.Mvc;
+using System.Web.Security;
+using Company.Web.Models;
+
+namespace Company.Web.Controllers
+{
+    [Authorize]
+    public class SecureController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            ViewBag.Message = "login";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            if (login.UserName == "test")
+            {
+                FormsAuthentication.SetAuthCookie(login.UserName, true);
+                return RedirectToAction("Home", "Secure");
+            }
+
+            ViewBag.Message = "login";
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Secure");
+        }
+
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Welcome User " + User.Identity.Name;
+
+            return View();
+        }
+    }
+}
