ba54ec82d2d52be4ffaba2a19fef6e0e6c59f6b2
Mohanraj Lakshmikanthan <mohanrajl@users.noreply.github.com>
2019-02-22 12:51:36 +0000

    Add files via upload

---
+++ Company.Models/Purchase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Company.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal NetAmount { get; set; }
+
+        public bool VatApplied { get; set; }
+
+        public bool Active { get; set; }
+
+        public decimal VatAmount
+        {
+            get
+            {
+                if (VatApplied)
+                {
+                    return (this.NetAmount / 20) * 100;
+                }
+
+                return 0;
+            }
+        }
+
+        public decimal GrossAmout
+        {
+            get
+            {
+                return this.NetAmount + this.VatAmount;
+            }
+        }
+    }
+}
